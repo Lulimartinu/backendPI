ec2b26000e57e14cbe7209b0c37fac41358c0550
Lulimartinu <lulimartinucci@hotmail.com>
2024-02-21 19:07:37 -0300

    interfaces servicio

---
+++ backend/backend/src/main/java/com/dh/Xplorando/service/ICategoriaService.java
@@ -0,0 +1,10 @@
+package com.dh.Xplorando.service;
+
+import com.dh.Xplorando.dto.salida.CategoriaSalidaDto;
+
+import java.util.List;
+
+public interface ICategoriaService {
+
+    List<CategoriaSalidaDto> detallarCategorias();
+}
---
+++ backend/backend/src/main/java/com/dh/Xplorando/service/IProductoService.java
@@ -0,0 +1,16 @@
+package com.dh.Xplorando.service;
+
+import com.dh.Xplorando.dto.entrada.ProductoEntradaDto;
+import com.dh.Xplorando.dto.salida.ProductoSalidaDto;
+import org.apache.coyote.BadRequestException;
+
+import java.util.List;
+
+public interface IProductoService {
+    List<ProductoSalidaDto> listarProductos();
+
+    ProductoSalidaDto crearProducto(ProductoEntradaDto producto) throws BadRequestException;
+    void eliminarProductoPorId(Long id) ;
+    ProductoSalidaDto buscarProductoPorId(Long id);
+
+}
