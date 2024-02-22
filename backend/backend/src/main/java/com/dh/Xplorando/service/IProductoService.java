8194235636d013b3205bb8cf150a3ca822c42d6d
Lulimartinu <lulimartinucci@hotmail.com>
2024-02-22 14:57:00 -0300

    agrego exceptions a interfaz de service

--- backend/backend/src/main/java/com/dh/Xplorando/service/IProductoService.java
+++ backend/backend/src/main/java/com/dh/Xplorando/service/IProductoService.java
@@ -2,6 +2,7 @@ package com.dh.Xplorando.service;
 
 import com.dh.Xplorando.dto.entrada.ProductoEntradaDto;
 import com.dh.Xplorando.dto.salida.ProductoSalidaDto;
+import com.dh.Xplorando.exceptions.ResourceNotFoundException;
 import org.apache.coyote.BadRequestException;
 
 import java.util.List;
@@ -9,7 +10,7 @@ import java.util.List;
 public interface IProductoService {
    List<ProductoSalidaDto> listarProductos();
     ProductoSalidaDto crearProducto(ProductoEntradaDto producto) throws BadRequestException;
-    void eliminarProductoPorId(Long id) ;
-   ProductoSalidaDto buscarProductoPorId(Long id);
+    void eliminarProductoPorId(Long id) throws ResourceNotFoundException;
+   ProductoSalidaDto buscarProductoPorId(Long id)throws ResourceNotFoundException;
 
 }
