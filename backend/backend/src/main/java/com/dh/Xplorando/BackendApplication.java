c06d72e918ef78fefd1466b87ea0d1d88cc3deb0
Lulimartinu <lulimartinucci@hotmail.com>
2024-02-24 19:08:22 -0300

    verifica conexion con h2

--- backend/backend/src/main/java/com/dh/Xplorando/BackendApplication.java
+++ backend/backend/src/main/java/com/dh/Xplorando/BackendApplication.java
@@ -29,5 +29,7 @@ public class BackendApplication {
 		return new ModelMapper();
 	}
 
+
+
 }
 
--- backend/backend/src/main/java/com/dh/Xplorando/controller/ProductoController.java
+++ backend/backend/src/main/java/com/dh/Xplorando/controller/ProductoController.java
@@ -5,23 +5,34 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("/productos")
-@CrossOrigin
+
 //crossOrigin ?
 public class ProductoController {
     //El controler es el responsable de manejar las solicitudes HTTP y enviar respuestas
 
     //es el artefacto principal de nuestros servicios REST
 
-@Autowired
+
     private final IProductoService productoService;
     public ProductoController(IProductoService productoService){
         this.productoService = productoService;
     }
+
+
 
 
 }
